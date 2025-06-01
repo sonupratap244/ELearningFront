87d4f592e4f9dba100fb11cc9c6f93a7e355c0a8
Son Pratap <117804069+sonupratap244@users.noreply.github.com>
2025-06-01 18:57:37 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from './App.jsx'
 import { UserContextProvider } from './context/UserContext.jsx'
 import { CourseContextProvider } from './context/CourseContext.jsx';
 
-export const server = "https://elearnback.onrender.com";
+export const server = "https://elearn-ebgm.onrender.com";
 
 
 createRoot(document.getElementById('root')).render(
